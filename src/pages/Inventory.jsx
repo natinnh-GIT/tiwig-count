a046774d90ff5ad3685e3b1165b2f121c279c58e
Nat In NH <natinnh@gmail.com>
2026-03-23 21:38:34 +0000

    File changes

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { base44 } from "@/api/base44Client";
-import { Plus, Search, Package, Download, LayoutDashboard } from "lucide-react";
+import { Plus, Search, Package, Download, LayoutDashboard, X } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import CategoryFilter from "@/components/CategoryFilter";
